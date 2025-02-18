d578743b9b2cd4633e26fbdb0254cece0a1ce4e5
Enrong Pan Peter <enrongpan@outlook.com>
2025-02-18 16:51:19 -0500

    first commit
    start the html code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Enrong-Pan-Personal-Website
+
---
+++ resume.c
@@ -0,0 +1,22 @@
+const coder = {
+    name:'Enrong Pan'
+    skills:[ 'Python', 'C', 'C++',
+        'Java', 'JavaScript', 'HTML',
+        'React', 'Boostrap', 'CSS',
+        'MySQL', 'MATLAB', 'R',
+        'GitHub', 'Bash', 
+    ],
+    hardWorker: true,
+    quickLearner: true,
+    teamPlayer: true,
+    problemSolver: true,
+    motivated: true,
+    interests: function() {
+        return (
+            this.Piano_Playing &&
+            this.Badminton &&
+            this.Photographing &&
+            this.Traveling
+        );
+    };
+};
